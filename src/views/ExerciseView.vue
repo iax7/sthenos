43c2b2d1ae1946651a2afdfc1130b244ec712521
Isaias Piña <iax7@users.noreply.github.com>
2025-10-29 21:11:56 -0600

    fix excerciseView classes

--- src/views/ExerciseView.vue
+++ src/views/ExerciseView.vue
@@ -21,13 +21,13 @@ function goBack() {
 </script>
 
 <template>
-  <div class="mx-auto w-full max-w-3xl px-4 py-6">
+  <div class="max-w-4xl">
     <div class="mb-4 flex items-center justify-between">
-      <h1 class="text-xl font-semibold">
+      <h2>
         {{ isEdit ? t('exercise.editor.titleEdit') : t('exercise.editor.titleNew') }}
-      </h1>
+      </h2>
       <BaseButton variant="secondary" type="button" @click="goBack">
-        <ArrowLeftCircleIcon class="w-5 h-5 mr-1" />
+        <ArrowLeftCircleIcon class="size-5 mr-1" />
         <span>{{ t('app.back') }}</span>
       </BaseButton>
     </div>
